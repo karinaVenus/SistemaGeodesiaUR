18523dbef18463eeb8163641c74dd2daabbb3de8
mainamax96 <alexander_pj_96@hotmail.com>
2022-07-02 23:53:54 -0500

    validacion from librerias

--- app/Http/Requests/FormReg_sal_cab.php
+++ app/Http/Requests/FormReg_sal_cab.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class FormReg_sal_cab extends FormRequest
 {
