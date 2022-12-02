1094684a63911e511a18b92c537020ec477a1322
Karina Pino Rojas <caro.rojas98494@outlook.com>
2022-12-02 18:33:30 -0500

    ultimopower

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login','App\Http\Controllers\UsuarioController@login');
 
-Route::group(['middleware'=>["auth:sanctum"]],function(){
+Route::group(['middleware'=>["auth:sanctum","cors"]],function(){
 //usuario  ********
     Route::get('/logout','App\Http\Controllers\UsuarioController@logout');
     //Route::get('/perfil/{id}','App\Http\Controllers\UsuarioController@profile');
